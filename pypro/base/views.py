a35fcb7906ddd179b96161796470bfd4c87918ec
luizfernando <luizfernandosmh@hotmail.com>
2020-09-21 17:16:43 -0300

    a

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Ola, Django')
+    return HttpResponse('<html><body>Ola, Django</body></html>', content_type='text/html')
