62a0b09047feb8e5cc273c2d1561bca5021fdfa0
luizfernando <luizfernandosmh@hotmail.com>
2020-09-28 17:02:10 -0300

    a

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 # from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<html><body>Ola, Django</body></html>', content_type='text/html')
+    return render(request, 'base/home.html')
