2605ecbd99c282c53d465d39b422adc0137e2765
luizfernando <luizfernandosmh@hotmail.com>
2020-11-03 21:53:39 -0300

    a

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Video Aperitivo: Motivacao', 'vimeo_id': 474442176}
+    return render(request, 'aperitivos/video.html', context={'video': video})
